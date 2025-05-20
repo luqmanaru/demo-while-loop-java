0083a9a228fa3ff27d600823616862124ab85cdb
Hanif Luqmanul Hakim <hanifluqmanulhakim20@gmail.com>
2025-05-20 13:16:32 +0700

    Create demo-while-loop.java

---
+++ demo-while-loop.java
@@ -0,0 +1,18 @@
+package com.mycompany.strukturkontrolpemilihan;
+
+/**
+ * Demonstrasi While Loop
+ * @author hp
+ * @contributor luqmanaru
+ */
+public class PerulanganWhile {
+    public static void main(String[] args) {
+        int x = 0; // Inisialisasi counter
+        
+        // While loop dengan kondisi
+        while (x < 10) {
+            System.out.println(x);
+            x++; // Increment counter
+        }
+    }
+}
